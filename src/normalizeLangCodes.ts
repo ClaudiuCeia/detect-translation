fdf94addb7c982273141da898caa6708149b9fe2
Duncan MacKenzie <d.mackenzie@elsevier.com>
2020-10-15 15:19:15 +0100

    Only normalize non-standard lang tags that tools specify in lang tags or to identify localized translations

--- src/normalizeLangCodes.ts
+++ src/normalizeLangCodes.ts
@@ -1,40 +1,25 @@
+// These regular expressions start with ^ as language subtags must appear at the start of a
+// compliant language tag, and most end with \b (word boundary) as this will match any tag
+// that either ends with the string, or continues with other subtags.
+
+// For example, /^tl\b/ will match both 'tl' (Tagalog) and also 'tl-PH' (Tagalog as spoken
+// in the Philippines), but not 'tly' or 'tly-AZ' (Talysh language as spoken in Azerbaijan).
+
 const NORMALISE_LANG_MAPPING = {
-  ara: 'ar', // Arabic (Baidu)
-  bul: 'bg', // Bulgarian (Baidu)
-  'bs-Latn': 'bs', // Bosnian (Sogou)
-  cat: 'ca', // Catalan (Apertium)
-  cat_valencia: 'ca-valencia', // Catalan (Valencia) (Apertium)
-  cht: 'zh-Hant', // Traditional Chinese (Baidu)
-  eng: 'en', // English (Apertium, Gramtrans)
-  est: 'et', // Estonian (Baidu)
-  epo: 'eo', // Esperanto (Apertium, Gramtrans)
-  fin: 'fi', // Finnish (Baidu)
-  fra: 'fr', // French (Baidu)
-  glg: 'gl', // Galician (Apertium)
-  dan: 'da', // Danish (Baidu, Gramtrans)
-  deu: 'de', // German (Gramtrans)
-  'eng/us': 'en-US', // English (US) (Gramtrans)
-  'eng/uk': 'en-GB', // English (UK) (Gramtrans)
-  jw: 'jv', // Javanese (Google)
-  kor: 'ko', // Korean (Baidu)
-  nor: 'no', // Norwegian (Gramtrans)
-  rom: 'ro', // Romanian (Baidu)
-  slo: 'sk', // Slovakian - NOTE `slo` is for SLOVAKIAN (sk) but Baidu means SLOVENIAN (sl), so we have custom code for Baidu in detect-language.js just for this case
-  swe: 'sv', // Swedish (Baidu, Gramtrans)
-  spa: 'es', // Spanish (Apertium, Baidu)
-  qax: 'eo', // Esperanto (VISL) (Gramtrans)
-  'zh-CHS': 'zh', // Simplified Chinese (Sogou, Youdao) (Hans does not need to be specified)
-  'zh-CHT': 'zh-Hant', // Traditional Chinese (Sogou) (Hant should be specified)
-  'zh-CN': 'zh',
-  'zh-TW': 'zh-Hant',
-  iw: 'he', // Hebrew (Google)
-  mww: 'hmn', // Hmong Daw; hmn is the inclusive code for Hmong (Bing/Sogou)
-  tl: 'fil', // Filipino (fil is more correct for the official language of the Philippines)
-  kazlat: 'kk-Latn', // Kazakh (Latin) (Yandex)
-  'sr-Cyrl': 'sr', // Serbian (Cyrillic is the default in Serbia)
-  usbcyr: 'uz-Cyrl', // Uzbek (Cyrillic) (Yandex)
-  vie: 'vi', // Vietnamese (Baidu)
-  wyw: 'lzh', // Classical/Literary Chinese (Baidu; wyw from 文言文, Wényánwén)
+  bs: /^bs-Latn\b/, // Bosnian (Sogou)
+  "ca-valencia": /^cat_valencia\b/, // Catalan (Valencia) (Apertium)
+  "en-US": /^eng\/us$/, // English (US) (Gramtrans)
+  "en-GB": /^eng\/uk$/, // English (UK) (Gramtrans)
+  he: /^iw\b/, // Hebrew (Google)
+  // note: if a service IDs something as ku (Kurdish - but Northern Kurdish in Latin script
+  // by default as per CLDR Likely Subtags), we need to check against the lang IDs as it
+  // might actually be ckb (Central Kurdish, written in Arabic script)
+  ku: /^kmr\b/, // Kurdish (Northern) (Bing) - the default for macrolanguage subtag ku
+  hmn: /^mww\b/, // Hmong Daw; hmn is the inclusive code for Hmong (Bing/Sogou)
+  fil: /^tl\b/, // Filipino (fil is more correct for the official language of the Philippines)
+  "kk-Latn": /^kazlat\b/, // Kazakh (Latin) (Yandex)
+  sr: /^sr-Cyrl\b/, // Serbian (Cyrillic is the default in Serbia)
+  "uz-Cyrl": /^usbcyr\b/, // Uzbek (Cyrillic) (Yandex)
 };
 
 /**
@@ -43,12 +28,13 @@ const NORMALISE_LANG_MAPPING = {
  * @param {string} lang - input language code
  * @returns {string} normalised language code
  */
-const normaliseLangCode = (lang) => {
-  const [, normalisedTag] = Object.entries(NORMALISE_LANG_MAPPING).find(
-    ([nonstd]) => lang.startsWith(nonstd),
-  ) || [];
+const normaliseLangCode = (lang: string): string => {
+  const [normalisedTag, nonstd] =
+    Object.entries(NORMALISE_LANG_MAPPING).find(([, nonstd]) =>
+      nonstd.test(lang)
+    ) || [];
 
-  return normalisedTag || lang;
+  return normalisedTag ? lang.replace(nonstd as RegExp, normalisedTag) : lang;
 };
 
 export default normaliseLangCode;
